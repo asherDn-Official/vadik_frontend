64dee6163f64ff160847ebb6654ecd5d085318df
nelvin <170600761+asherDn-Official@users.noreply.github.com>
2025-06-20 18:00:21 +0530

    corection

--- src/components/customerProfile/CustomerDetails.jsx
+++ src/components/customerProfile/CustomerDetails.jsx
@@ -266,10 +266,10 @@ const CustomerDetails = ({
                   <button
                     key={tab}
                     onClick={() => setActiveTab(tab)}
-                    className={`py-4 px-1 border-b-2 font-medium rounded-[10px] text-sm ${
+                    className={`py-4 px-4 border-b-2 font-medium rounded-[10px] text-sm ${
                       activeTab === tab
                         ? "bg-[#EC396F1A] text-[#EC396F]"
-                        : "border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300"
+                        : "border-transparent text-gray-500 hover:text-[#EC396F] hover:bg-[#EC396F1A]"
                     }`}
                   >
                     {tab}
