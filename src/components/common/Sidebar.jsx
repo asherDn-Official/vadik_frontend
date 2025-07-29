1236b4ce9735713334cedcaee8620208102e5e2d
GANGADHARAN-ls <developers@asherdn.com>
2025-07-29 12:48:30 +0530

    Update Sidebar.jsx

--- src/components/common/Sidebar.jsx
+++ src/components/common/Sidebar.jsx
@@ -112,8 +112,8 @@ function Sidebar() {
           );
         })}
         <button onClick={handleLogout} className=" flex flex-1 w-full items-center px-6 py-5 text-white no-underline transition-colors hover:bg-[#3d3b83]">
-          <LogOut size={20} className="mr-3" />
-          Logout
+          <LogOut size={20} className="mr-4" />
+          &nbsp;Logout
         </button>
       </nav>
 
