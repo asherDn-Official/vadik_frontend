adae088085225c4020bbb7e71e5062ce64f03b83
praveen <developers@asherdn.com>
2025-08-13 18:46:42 +0530

    Update SpinWheelForm.jsx

--- src/components/customeroppertunites/SpinWheelForm.jsx
+++ src/components/customeroppertunites/SpinWheelForm.jsx
@@ -521,7 +521,7 @@ const SpinWheelForm = ({ campaign, onSave, onCancel }) => {
             type="submit"
             className="px-6 py-2 bg-pink-600 text-white rounded-lg hover:bg-pink-700 transition-colors"
           >
-            Save Spin Wheel
+            {campaign ? 'Save Spin Wheel' : 'Create Spin Wheel'} 
           </button>
         </div>
       </form>
