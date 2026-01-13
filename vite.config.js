ad098b901815a19be5694c5d99b472da60285bbb
GANGADHARAN26 <developers@asherdn.com>
2026-01-13 13:43:56 +0530

    Revert "Merge branch 'Own-Number-Integration-FE'"
    This reverts commit f055ff403d5284eb1458b9740eb277bacef8754c, reversing
    changes made to 776edd78c9d84865294c16a6fa2dbce0fcdd46d1.

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-    server: {
-    allowedHosts: [
-      "0e907ea7340c.ngrok-free.app"
-    ],
-  },
 })
